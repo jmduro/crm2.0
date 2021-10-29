471cb1b28da23c548e111ca353d36063de232378
Alejojooo <alejandrofn2204@gmail.com>
2021-10-29 10:40:47 -0600

    Base URLs

--- crm2/urls.py
+++ crm2/urls.py
@@ -1,21 +1,37 @@
-"""crm2 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.views import (LoginView,
+                                       LogoutView,
+                                       PasswordResetView,
+                                       PasswordResetDoneView,
+                                       PasswordResetConfirmView,
+                                       PasswordResetCompleteView)
+from django.urls import path, include
+from users.views import LandingPageView, SignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPageView.as_view(), name='landing-page'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    # path('users/', include('users.urls', namespace="users")),
+    # path('products/', include('products.urls', namespace="products")),
+    # path('events/', include('events.urls', namespace="events")),
+    path('companies/', include('companies.urls', namespace='companies')),
+    # path('contacts/', include('contacts.urls', namespace='contacts')),
+    # path('tasks/', include('tasks.urls', namespace='tasks')),
+    # path('deals/', include('deals.urls', namespace="deals")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)

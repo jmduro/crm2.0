8c06db401cb951ba8d71a3b34523bc111b57d98e
Alejojooo <alejandrofn2204@gmail.com>
2021-11-05 10:08:23 -0600

    Update models.py

--- contacts/models.py
+++ contacts/models.py
@@ -6,17 +6,19 @@ from companies.models import Company
 
 
 class Contact(models.Model):
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    title = models.CharField(max_length=50)
-    email = models.EmailField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(blank=True)
     company_name = models.ForeignKey(
         Company, null=True, blank=True, on_delete=models.SET_NULL)
-    mobile = models.CharField(max_length=20)
-    phone_number = models.CharField(max_length=20)
-    home_phone = models.CharField(max_length=20)
-    description = models.TextField()
+    mobile = models.CharField(max_length=25, blank=True)
+    phone = models.CharField(max_length=25, blank=True)
+    home_phone = models.CharField(max_length=25, blank=True)
+    email_opt_out = models.EmailField(blank=True)
+    description = models.TextField(max_length=250, blank=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'

e850166895e7c4b43e0ef8db28947f19253a1b37
jmduro <jmdubonx@gmail.com>
2021-11-05 15:22:03 -0600

    htmls-views

--- crm2/settings.py
+++ crm2/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     #Models
     'companies',
     'contacts',
+    'products',
     'tasks',
     'events',
     'users',
---
+++ products/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from users.models import User
+
+# Create your models here.
+
+class Product(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=50, unique=True)
+    product_code = models.CharField(max_length=50, blank=True)
+    # Pendiente función categoría
+    product_category = models.CharField(max_length=20)
+    unit_price = models.FloatField(default=0)
+    description = models.TextField(max_length=250, blank=True)
+    product_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.product_name}'

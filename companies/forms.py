75892df1fa93ff2da342c12d758bb20419f1a6e2
Alejojooo <alejandrofn2204@gmail.com>
2021-11-05 06:48:51 -0600

    Last update for views

--- companies/forms.py
+++ companies/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 from .models import *
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column, Field, Div
+from crispy_forms.bootstrap import InlineCheckboxes
+
+Field.template = 'custom_fields/custom_field.html'
 
 
 class CompanyModelForm(forms.ModelForm):
@@ -16,4 +21,25 @@ class CompanyModelForm(forms.ModelForm):
             'billing_country',
             'billing_code',
             'featured',
+        )
+        # help_texts = {
+        #     'company_name' : 'name'
+        # }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.label_class = 'block text-purple-700 text-sm font-bold mb-2'
+        self.helper.layout = Layout(
+            'company_name',
+            'phone',
+            'website',
+            'description',
+            'billing_street',
+            'billing_city',
+            'billing_state',
+            'billing_country',
+            'billing_code',
+            'featured',
+            Submit('submit', 'Submit', css_class="flex px-6 py-2 ml-auto text-white bg-purple-500 border-0 rounded focus:outline-none hover:bg-purple-600")
         )

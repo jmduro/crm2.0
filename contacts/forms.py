110e886927f9329276b6ec8f8c1aa9c8e5c4bdc2
Alejojooo <alejandrofn2204@gmail.com>
2021-11-05 10:24:28 -0600

    Custom crispy forms

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from .models import Contact
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
 
 User = get_user_model()
+
 
 class ContactModelForm(forms.ModelForm):
     class Meta:
@@ -18,4 +21,22 @@ class ContactModelForm(forms.ModelForm):
             'home_phone',
             'email_opt_out',
             'description',
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.label_class = 'block text-red-700 text-sm font-bold mb-2'
+        self.helper.layout = Layout(
+            'first_name',
+            'last_name',
+            'title',
+            'email',
+            'company_name',
+            'mobile',
+            'phone',
+            'home_phone',
+            'email_opt_out',
+            'description',
+            Submit('submit', 'Submit', css_class="flex px-6 py-2 ml-auto text-white bg-red-500 border-0 rounded focus:outline-none hover:bg-purple-600")
         )

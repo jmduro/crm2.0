1387d43c20ed2d3d5e0f21e6c3bc4dad379cb05e
Alejojooo <alejandrofn2204@gmail.com>
2021-11-05 10:04:30 -0600

    Preview

--- leads/urls.py
+++ leads/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
     path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
     path('create/', LeadCreateView.as_view(), name='lead-create'),
+    path('<int:pk>/assign-agent/', AssignAgentView.as_view(), name='assign-agent'),
+    path('<int:pk>/category/', LeadCategoryUpdateView.as_view(), name='lead-category-update'),
+    path('categories/', CategoryListView.as_view(), name='category-list'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+    path('search/', search_lead, name = 'lead-search')
 ]
